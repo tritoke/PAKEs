9d222df1bc431bc4874acb8b1e52bc2099a37272
Sam <tritoke@protonmail.com>
2023-02-07 01:35:37 +0000

    ✨ Added no_std example.

--- aucpace/examples/key_agreement_no_std.rs
+++ aucpace/examples/key_agreement_no_std.rs
@@ -1,12 +1,14 @@
 #![no_std]
 
+// use println only from std
+extern crate std;
+use std::println;
+
 use aucpace::{Client, ClientMessage, Database, Result, Server, ServerMessage};
 use curve25519_dalek::ristretto::RistrettoPoint;
 use password_hash::{ParamsString, SaltString};
 use rand_core::OsRng;
 use scrypt::{Params, Scrypt};
-use sha2::digest::Output;
-use sha2::Sha512;
 
 /// function like macro to wrap sending data over a tcp stream, returns the number of bytes sent
 macro_rules! send {
@@ -20,14 +22,14 @@ macro_rules! send {
 /// function like macro to wrap receiving data over a tcp stream, returns the message received
 macro_rules! recv {
     ($buf:ident) => {{
-        postcard::from_slice(&$buf).unwrap()
+        postcard::from_bytes(&$buf).unwrap()
     }};
 }
 
 fn main() -> Result<()> {
     // example username and password, never user these...
-    const USERNAME: &'static [u8] = b"jlpicard_1701";
-    const PASSWORD: &'static [u8] = b"g04tEd_c4pT41N";
+    const USERNAME: &'static [u8] = b"adira.tal";
+    const PASSWORD: &'static [u8] = b"4d1rA_aND-Gr4Y_aRe_tH3-b3sT <3";
 
     // register the user in the database
     let mut base_server = Server::new(OsRng);
@@ -35,7 +37,7 @@ fn main() -> Result<()> {
     let mut database: SingleUserDatabase<100> = Default::default();
 
     let params = Params::recommended();
-    let registration = base_client.register_alloc(USERNAME, PASSWORD, params, Scrypt)?;
+    let registration = base_client.register::<&[u8], 100>(USERNAME, PASSWORD, params, Scrypt)?;
     if let ClientMessage::Registration {
         username,
         salt,
@@ -65,7 +67,7 @@ fn main() -> Result<()> {
 
     // server receives SSID establishment packet and begins SSID establishment itself,
     // responding with a ServerNonce packet
-    let mut client_message: ClientMessage<16> = recv!(client_buf, buf);
+    let mut client_message: ClientMessage<16> = recv!(client_buf);
     let server = if let ClientMessage::Nonce(client_nonce) = client_message {
         let (server, message) = base_server.begin();
         let bytes_sent = send!(server_buf, message);
@@ -80,9 +82,9 @@ fn main() -> Result<()> {
     };
 
     // the client receives the nonce, calculates the SSID then moves on to the
-    server_message = recv!(server_buf);
-    let (server, message) = if let ServerMessage::Nonce(username) = client_message {
-        server.generate_client_info(username, &database, OsRng)
+    let mut server_message = recv!(server_buf);
+    let client = if let ServerMessage::Nonce(nonce) = server_message {
+        client.agree_ssid(nonce)
     } else {
         panic!("Received invalid client message {:?}", client_message);
     };
@@ -104,7 +106,7 @@ fn main() -> Result<()> {
     } else {
         panic!("Received invalid client message {:?}", client_message);
     };
-    let bytes_sent = send!(stream, message);
+    let bytes_sent = send!(server_buf, message);
     server_bytes_sent += bytes_sent;
     println!(
         "[server] Sending message: AugmentationInfo, sent {} bytes",
@@ -128,135 +130,93 @@ fn main() -> Result<()> {
 
             Params::new(log_n, r, p).unwrap()
         };
-        client.generate_cpace_alloc(x_pub, PASSWORD, &salt, params, Scrypt)?
+        client.generate_cpace::<&[u8], &SaltString, 100>(x_pub, PASSWORD, &salt, params, Scrypt)?
     } else {
         panic!("Received invalid server message {:?}", server_message);
     };
 
     // ===== CPace substep =====
-    // let ci = TcpChannelIdentifier::new(client_addr, server_socket).unwrap();
-    // let (server, message) = server.generate_public_key(ci);
-    // let bytes_sent = send!(stream, message);
-    // server_bytes_sent.fetch_add(bytes_sent, Ordering::SeqCst);
-    // println!(
-    //     "[server] Sending message: PublicKey, sent {} bytes",
-    //     bytes_sent
-    // );
+    // first generate the server's public key and send it
+    const CI: &'static str = "channel_identifier";
+    let (server, message) = server.generate_public_key(CI);
+    let bytes_sent = send!(server_buf, message);
+    server_bytes_sent += bytes_sent;
+    println!(
+        "[server] Sending message: PublicKey, sent {} bytes",
+        bytes_sent
+    );
 
-    // client_message = recv!(stream, buf);
-    // let server = if let ClientMessage::PublicKey(client_pubkey) = client_message {
-    //     server.receive_client_pubkey(client_pubkey)
-    // } else {
-    //     panic!("Received invalid client message {:?}", client_message);
-    // };
+    // now generate the client's public key and send it
+    let (client, message) = client.generate_public_key(CI, &mut OsRng);
+    let bytes_sent = send!(client_buf, message);
+    client_bytes_sent += bytes_sent;
+    println!(
+        "[client] Sending message: PublicKey, sent {} bytes",
+        bytes_sent
+    );
+
+    // now receive the client's public key
+    client_message = recv!(client_buf);
+    let server = if let ClientMessage::PublicKey(client_pubkey) = client_message {
+        server.receive_client_pubkey(client_pubkey)
+    } else {
+        panic!("Received invalid client message {:?}", client_message);
+    };
+
+    // now receive the server's public key
+    server_message = recv!(server_buf);
+    let (client, message) = if let ServerMessage::PublicKey(server_pubkey) = server_message {
+        client.receive_server_pubkey(server_pubkey)
+    } else {
+        panic!("Received invalid server message {:?}", server_message);
+    };
 
     // ===== Explicit Mutual Authentication =====
-    // client_message = recv!(stream, buf);
-    // if let ClientMessage::ClientAuthenticator(client_authenticator) = client_message {
-    //     let (key, message) = server.receive_client_authenticator(client_authenticator)?;
-    //     let bytes_sent = send!(stream, message);
-    //     server_bytes_sent.fetch_add(bytes_sent, Ordering::SeqCst);
-    //     println!(
-    //         "[server] Sending message: ServerAuthenticator, sent {} bytes",
-    //         bytes_sent
-    //     );
+    // send the client's authenticator
+    let bytes_sent = send!(client_buf, message);
+    client_bytes_sent += bytes_sent;
+    println!(
+        "[client] Sending message: Authenticator, sent {} bytes",
+        bytes_sent
+    );
 
-    //     // return the dervied key
-    //     Ok(key)
-    // } else {
-    //     panic!("Received invalid client message {:?}", client_message);
-    // }
+    // receive the client's authenticator
+    client_message = recv!(client_buf);
+    let server_key = if let ClientMessage::Authenticator(client_authenticator) = client_message {
+        let (key, message) = server.receive_client_authenticator(client_authenticator)?;
+        let bytes_sent = send!(server_buf, message);
+        server_bytes_sent += bytes_sent;
+        println!(
+            "[server] Sending message: Authenticator, sent {} bytes",
+            bytes_sent
+        );
 
-    // let mut server_buf = [0u8; 1024];
+        // return the dervied key
+        key
+    } else {
+        panic!("Received invalid client message {:?}", client_message);
+    };
 
-    // // ===== SSID ESTABLISHMENT =====
+    // receive the server's authenticator
+    server_message = recv!(server_buf);
+    let client_key = if let ServerMessage::Authenticator(server_authenticator) = server_message {
+        client
+            .receive_server_authenticator(server_authenticator)
+            .unwrap()
+    } else {
+        panic!("Received invalid server message {:?}", server_message);
+    };
 
-    // receive the server nonce to agree on SSID
-    // let mut server_message: ServerMessage<16> = recv!(stream, buf);
-    // let client = if let ServerMessage::ServerNonce(server_nonce) = server_message {
-    // client.agree_ssid(server_nonce)
-    // } else {
-    // panic!("Received invalid server message {:?}", server_message);
-    // };
-
-    // ===== Augmentation Layer =====
-    // let (client, message) = client.start_augmentation(USERNAME);
-    // let bytes_sent = send!(stream, message);
-    // client_bytes_sent.fetch_add(bytes_sent, Ordering::SeqCst);
-    // println!(
-    // "[client] Sending message: Username, sent {} bytes",
-    // bytes_sent
-    // );
-
-    // server_message = recv!(stream, buf);
-    // let client = if let ServerMessage::AugmentationInfo {
-    // x_pub,
-    // salt,
-    // pbkdf_params,
-    // ..
-    // } = server_message
-    // {
-    // let params = {
-    //     // its christmas time!
-    //     let log_n = pbkdf_params.get_str("ln").unwrap().parse().unwrap();
-    //     let r = pbkdf_params.get_str("r").unwrap().parse().unwrap();
-    //     let p = pbkdf_params.get_str("p").unwrap().parse().unwrap();
-
-    //     Params::new(log_n, r, p).unwrap()
-    // };
-    // client.generate_cpace_alloc(x_pub, PASSWORD, &salt, params, Scrypt)?
-    // } else {
-    // panic!("Received invalid server message {:?}", server_message);
-    // };
-
-    // ===== CPace substep =====
-    // let ci = TcpChannelIdentifier::new(stream.local_addr().unwrap(), server_socket).unwrap();
-    // let (client, message) = client.generate_public_key(ci, &mut OsRng);
-    // let bytes_sent = send!(stream, message);
-    // client_bytes_sent.fetch_add(bytes_sent, Ordering::SeqCst);
-    // println!(
-    // "[client] Sending message: PublicKey, sent {} bytes",
-    // bytes_sent
-    // );
-
-    // server_message = recv!(stream, buf);
-    // let (client, message) = if let ServerMessage::PublicKey(server_pubkey) = server_message {
-    // client.receive_server_pubkey(server_pubkey)
-    // } else {
-    // panic!("Received invalid server message {:?}", server_message);
-    // };
-
-    // ===== Explicit Mutual Auth =====
-    // let bytes_sent = send!(stream, message);
-    // client_bytes_sent.fetch_add(bytes_sent, Ordering::SeqCst);
-    // println!(
-    // "[client] Sending message: ClientAuthenticator, sent {} bytes",
-    // bytes_sent
-    // );
-
-    // server_message = recv!(stream, buf);
-    // if let ServerMessage::ServerAuthenticator(server_authenticator) = server_message {
-    // client.receive_server_authenticator(server_authenticator)
-    // } else {
-    // panic!("Received invalid server message {:?}", server_message);
-    // }
-
+    // ===== Protocol end =====
     // assert that both threads arrived at the same key
-    // let client_key: Output<Sha512> = client_thread.join().unwrap().unwrap();
-    // let server_key: Output<Sha512> = server_thread.join().unwrap().unwrap();
-    // assert_eq!(client_key, server_key);
-    // println!(
-    //     "Negotiation finished, both parties arrived at a key of: {:X}",
-    //     client_key
-    // );
-    // println!(
-    //     "Client sent {} bytes total",
-    //     client_bytes_sent.load(Ordering::SeqCst)
-    // );
-    // println!(
-    //     "Server sent {} bytes total",
-    //     server_bytes_sent.load(Ordering::SeqCst)
-    // );
+    assert_eq!(client_key, server_key);
+    println!(
+        "Negotiation finished, both parties arrived at a key of: {:X}",
+        client_key
+    );
+
+    println!("Client sent {} bytes total", client_bytes_sent);
+    println!("Server sent {} bytes total", server_bytes_sent);
 
     Ok(())
 }
@@ -293,9 +253,9 @@ impl<const USERSIZE: usize> Database for SingleUserDatabase<USERSIZE> {
         params: ParamsString,
     ) {
         // silently fail because this is just an example and I'm lazy
-        if username.len() > USERSIZE {
+        if username.len() <= USERSIZE {
             let mut buf = [0u8; USERSIZE];
-            buf.copy_from_slice(username);
+            buf[..username.len()].copy_from_slice(username);
             self.user = Some((buf, username.len()));
             self.data = Some((verifier, salt, params));
         }
